197907cd9cb607cea98dd4171124c62f709acb7e
SiddAjriY2Jaccount <y2jsiddajri@gmail.com>
2023-06-20 17:45:51 -0700

    update with a6

--- app.js
+++ app.js
@@ -1,15 +1,20 @@
 import express, { json } from "express";
 import session from "express-session";
+import mongoose from "mongoose";
 import cors from "cors";
 import HelloController from "./controllers/hello-controller.js";
 import UserController from "./users/users-controller.js";
 import tuitsController from "./controllers/tuits/tuits-controller.js";
 import AuthController from "./users/auth-controller.js";
+import dotenv from "dotenv";
+dotenv.config();
+const CONNECTION_STRING =
+  process.env.DB_CONNECTION_STRING || "mongodb://127.0.0.1:27017/tuiter";
+// const CONNECTION_STRING = process.env.DB_CONNECTION_STRING;
+mongoose.connect(CONNECTION_STRING);
 
 const app = express();
 const PORT = 4000;
-const allowedOrigins = ['https://a5--jolly-gaufre-12c5d0.netlify.app', 'http://localhost:3000'];
-
 // Middleware
 app.use(json());
 app.use(
@@ -22,7 +27,7 @@ app.use(
 app.use(
   cors({
     credentials: true,
-    origin: allowedOrigins,
+    origin: "http://localhost:3000",
   })
 );
 
@@ -32,4 +37,6 @@ tuitsController(app);
 AuthController(app);
 
 // Start the server
-app.listen(process.env.PORT || 4000);
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
--- controllers/tuits/tuits-controller.js
+++ controllers/tuits/tuits-controller.js
@@ -1,5 +1,6 @@
-import posts from "./tuits.js";
-let tuits = posts;
+// import posts from "./tuits.js";
+// let tuits = posts;
+import * as tuitsDao from "./tuits-dao.js";
 
 export default (app) => {
   app.post("/api/tuits", createTuit);
@@ -8,29 +9,59 @@ export default (app) => {
   app.delete("/api/tuits/:tid", deleteTuit);
 };
 
-const createTuit = (req, res) => {
+// const createTuit = (req, res) => {
+//   const newTuit = req.body;
+//   newTuit._id = new Date().getTime() + "";
+//   newTuit.likes = 0;
+//   newTuit.liked = false;
+//   tuits.push(newTuit);
+//   res.json(newTuit);
+// };
+// const findTuits = (req, res) => {
+//   res.json(tuits);
+// };
+
+const createTuit = async (req, res) => {
   const newTuit = req.body;
-  newTuit._id = new Date().getTime() + "";
   newTuit.likes = 0;
   newTuit.liked = false;
-  tuits.push(newTuit);
-  res.json(newTuit);
-};
-const findTuits = (req, res) => {
-  res.json(tuits);
-};
-const updateTuit = (req, res) => {
-  const tuitdId = req.params.tid;
-  const updates = req.body;
-  const tuitIndex = tuits.findIndex((t) => t._id === tuitdId);
-  tuits[tuitIndex] = { ...tuits[tuitIndex], ...updates };
-  res.sendStatus(200);
+  newTuit.dislikes = 0;
+  newTuit.disliked = false;
+  const insertedTuit = await tuitsDao.createTuit(newTuit);
+  res.json(insertedTuit);
 };
 
-const deleteTuit = (req, res) => {
+const findTuits = async (req, res) => {
+  const tuits = await tuitsDao.findTuits();
+  res.json(tuits);
+};
+
+// const updateTuit = (req, res) => {
+//   const tuitdId = req.params.tid;
+//   const updates = req.body;
+//   const tuitIndex = tuits.findIndex((t) => t._id === tuitdId);
+//   tuits[tuitIndex] = { ...tuits[tuitIndex], ...updates };
+//   res.sendStatus(200);
+// };
+
+// const deleteTuit = (req, res) => {
+//   console.log(`delete called for ${req.params.tid}`);
+//   const tuitdIdToDelete = req.params.tid;
+//   tuits = tuits.filter((t) => t._id != tuitdIdToDelete);
+//   console.log(tuits);
+//   res.sendStatus(200);
+// };
+
+const deleteTuit = async (req, res) => {
   console.log(`delete called for ${req.params.tid}`);
   const tuitdIdToDelete = req.params.tid;
-  tuits = tuits.filter((t) => t._id != tuitdIdToDelete);
-  console.log(tuits);
-  res.sendStatus(200);
+  const status = await tuitsDao.deleteTuit(tuitdIdToDelete);
+  res.json(status);
+};
+
+const updateTuit = async (req, res) => {
+  const tuitdId = req.params.tid;
+  const updates = req.body;
+  const status = await tuitsDao.updateTuit(tuitdId, updates);
+  res.json(status);
 };
